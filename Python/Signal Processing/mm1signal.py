b9f5f7fd6e4c1557ff8ecbe6582fdb4f9eac89c5
Peter Plass Jensen <Petaarz@hotmail.com>
2023-09-07 13:58:16 +0200

    Bandpass Filter

--- Python/Signal Processing/mm1signal.py
+++ Python/Signal Processing/mm1signal.py
@@ -4,14 +4,30 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # Load the dataset into variable mat from .mat data file
-mat = scipy.io.loadmat('C:/Users/Petaa/Desktop/Skole/Programming/Python/Python-Programming/Python/Signal Processing/ME.mat')
+#mat = scipy.io.loadmat('C:/Users/Petaa/Desktop/Skole/Programming/Python/Python-Programming/Python/Signal Processing/ME.mat')
+
+# desktop directory;
+mat = scipy.io.loadmat('C:/Users/Petaa/Documents/GitHub/Python-Programming/Python/Signal Processing/ME.mat')
+
 #print(mat.items())
 
 # Variable czData contains the data recorded from electrode at cz in the brain (center)
 czData = np.array(mat['data'][4])
 
+# Sampling rate 500hz
 fs = 500
 
+# Sampling time
+epoch_start = 3
+
+# Highpass
+highpass = False
+
+# Range of cutoff frequency
+cutoff_freq = [0.005,5]
+
+# Order of filter
+order = 4
 
 # Plot the data vector per Exercise 1.1
 fig, axs = plt.subplots(1)
@@ -20,13 +36,17 @@ axs.plot(czData, color='C0')
 axs.set_xlabel("Time")
 axs.set_ylabel("Amplitude")
 
-
+czData = czData[:-1]
 # IIR filter from Exercise 1.2
-#filteredczDATA=scp.iirfilter(4,czData,btype=)
+b, a =scp.iirfilter(order, cutoff_freq, btype="bandpass", fs=fs , analog=False)
+filteredCzData = scp.lfilter(b,a,czData,axis=-1)
+
+
+
 # Plot the data vector per Exercise 1.2
 fig, ax = plt.subplots(1)
 ax.set_title("Signal")
-#ax.plot(filteredczDATA, color='C0')
+ax.plot(filteredCzData, color='C0')
 ax.set_xlabel("Time")
 ax.set_ylabel("Amplitude")
 
